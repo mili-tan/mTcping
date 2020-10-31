1217975536647bfdb74ca7d7bd1e27fdb507d589
Milkey Tan <819774989@qq.com>
2020-10-31 22:33:03 +0800

    sth

--- mTcping/Program.cs
+++ mTcping/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
 
@@ -21,7 +22,7 @@ namespace mTcping
                               $"Copyright (c) {DateTime.Now.Year} Milkey Tan. Code released under the Mozilla Public License 2.0"
             };
             cmd.HelpOption("-?|-h|--help");
-            var hostArgument = cmd.Argument("host", "指定的目标主机地址");
+            var hostArgument = cmd.Argument("host", "指定的目标主机地址。");
             var tOption = cmd.Option<string>("-t", "Tcping 指定的主机，直到键入 Ctrl+C 停止。", CommandOptionType.NoValue);
             var nOption = cmd.Option<int>("-n <count>", "要发送的回显请求数。", CommandOptionType.SingleValue);
             var wOption = cmd.Option<int>("-w <timeout>", "等待每次回复的超时时间(毫秒)。", CommandOptionType.SingleValue);
@@ -56,16 +57,16 @@ namespace mTcping
                         try
                         {
                             var socks = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-                                { Blocking = false, ReceiveTimeout = 3000, SendTimeout = 3000 };
+                                { Blocking = false, ReceiveTimeout = 2000, SendTimeout = 2000 };
                             var result = socks.BeginConnect(point, null, null);
-                            if (!result.AsyncWaitHandle.WaitOne(3000, true))
+                            if (!result.AsyncWaitHandle.WaitOne(2000, true))
                             {
                                 errors.Add(0);
                                 conn = false;
                             }
                             else
                             {
-                                socks.Close(3000);
+                                socks.Close(2000);
                             }
                         }
                         catch (Exception exception)
@@ -78,7 +79,8 @@ namespace mTcping
                         var time = Convert.ToInt32(stopWatch.Elapsed.TotalMilliseconds);
                         if (conn) times.Add(time);
                         Console.WriteLine($"来自 {point.Address}:{point.Port} 的 TCP 响应: 端口={conn} 时间={time}ms");
-                    }).Wait(6000);
+                        Thread.Sleep(500);
+                    }).Wait(5000);
                 }
             });
             cmd.Execute(args);
