70abbd8227ac0a1bda6153b991c70789884cd87f
Milkey Tan <819774989@qq.com>
2020-11-01 21:23:22 +0800

    statistics

--- mTcping/Program.cs
+++ mTcping/Program.cs
@@ -21,11 +21,16 @@ namespace mTcping
                               Environment.NewLine +
                               $"Copyright (c) {DateTime.Now.Year} Milkey Tan. Code released under the Mozilla Public License 2.0"
             };
+
             cmd.HelpOption("-?|-h|--help");
             var hostArgument = cmd.Argument("host", "指定的目标主机地址。");
             var tOption = cmd.Option<string>("-t", "Tcping 指定的主机，直到键入 Ctrl+C 停止。", CommandOptionType.NoValue);
             var nOption = cmd.Option<int>("-n <count>", "要发送的回显请求数。", CommandOptionType.SingleValue);
             var wOption = cmd.Option<int>("-w <timeout>", "等待每次回复的超时时间(毫秒)。", CommandOptionType.SingleValue);
+            var times = new List<int>();
+            var errors = new List<int>();
+            var sent = 0;
+
             cmd.OnExecute(() =>
             {
                 if (string.IsNullOrWhiteSpace(hostArgument.Value))
@@ -34,6 +39,7 @@ namespace mTcping
                     return;
                 }
 
+                Console.WriteLine();
                 var host = hostArgument.Value.Contains("://")
                     ? new Uri(hostArgument.Value)
                     : new Uri("http://" + hostArgument.Value);
@@ -41,32 +47,34 @@ namespace mTcping
                     ? new IPEndPoint(Dns.GetHostAddresses(host.Host).FirstOrDefault(), host.Port)
                     : new IPEndPoint(IPAddress.Parse(host.Host), host.Port);
                 Console.WriteLine($"正在 Tcping {point.Address}:{point.Port} 目标主机" +
-                                  (host.HostNameType == UriHostNameType.Dns ? $" [{host}]" : string.Empty) + ":");
+                                  (host.HostNameType == UriHostNameType.Dns ? $" [{host.Host}]" : string.Empty) + ":");
 
-                var times = new List<int>();
-                var errors = new List<int>();
                 for (var i = 0;
                     i < (nOption.HasValue() ? nOption.ParsedValue : tOption.HasValue() ? int.MaxValue : 4);
                     i++)
                 {
                     Task.Run(() =>
                     {
+                        sent += 1;
                         var stopWatch = new Stopwatch();
                         var conn = true;
                         stopWatch.Start();
                         try
                         {
                             var socks = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-                                { Blocking = false, ReceiveTimeout = 2000, SendTimeout = 2000 };
+                            {
+                                Blocking = false, ReceiveTimeout = wOption.HasValue() ? wOption.ParsedValue : 2000,
+                                SendTimeout = wOption.HasValue() ? wOption.ParsedValue : 2000
+                            };
                             var result = socks.BeginConnect(point, null, null);
-                            if (!result.AsyncWaitHandle.WaitOne(2000, true))
+                            if (!result.AsyncWaitHandle.WaitOne(wOption.HasValue() ? wOption.ParsedValue : 2000, true))
                             {
                                 errors.Add(0);
                                 conn = false;
                             }
                             else
                             {
-                                socks.Close(2000);
+                                socks.Close(wOption.HasValue() ? wOption.ParsedValue : 2000);
                             }
                         }
                         catch (Exception exception)
@@ -75,13 +83,23 @@ namespace mTcping
                             errors.Add(0);
                             conn = false;
                         }
+
                         stopWatch.Stop();
                         var time = Convert.ToInt32(stopWatch.Elapsed.TotalMilliseconds);
                         if (conn) times.Add(time);
                         Console.WriteLine($"来自 {point.Address}:{point.Port} 的 TCP 响应: 端口={conn} 时间={time}ms");
                         Thread.Sleep(500);
-                    }).Wait(5000);
+                    }).Wait(wOption.HasValue() ? wOption.ParsedValue + 1000 : 3000);
                 }
+
+                Console.WriteLine();
+                Console.WriteLine($"{point.Address}:{point.Port} 的 Tcping 统计信息:");
+                Console.WriteLine(
+                    $"    数据包: 已发送 = {sent}，已接收 = {times.Count}，失败 = {errors.Count} ({errors.Count / (double)sent:0%} 失败)");
+                if (times.Count <= 0) return;
+                Console.WriteLine("往返行程的估计时间(以毫秒为单位):");
+                Console.WriteLine(
+                    $"    最短 = {times.Min():0.0}ms，最长 = {times.Max():0.0}ms，平均 = {times.Average():0.0}ms");
             });
             cmd.Execute(args);
         }
